9d7729d2f36092e782b69e78360c07fa0af68d76
Ivan Navas <navass21112@gmail.com>
2025-08-29 15:11:40 +0200

    change the theme of code DEMO

--- src/app/utils/pipes/HighlightPipe.ts
+++ src/app/utils/pipes/HighlightPipe.ts
@@ -1,9 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import hljs from 'highlight.js';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-@Pipe({ name: 'highlight' })
+@Pipe({
+  name: 'highlight',
+})
 export class HighlightPipe implements PipeTransform {
-  transform(code: string): string {
-    return hljs.highlightAuto(code).value;
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(code: string, language: string = 'typescript'): SafeHtml {
+    let highlighted: string;
+
+    if (hljs.getLanguage(language)) {
+      highlighted = hljs.highlight(code, { language }).value;
+    } else {
+      highlighted = hljs.highlightAuto(code).value;
+    }
+
+    return this.sanitizer.bypassSecurityTrustHtml(highlighted);
   }
 }
