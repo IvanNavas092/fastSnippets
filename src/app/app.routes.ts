98f9392c8b1e959d8dc37b736e09b89b7f087197
IvanNavas092 <navass21112@gmail.com>
2025-09-27 18:21:20 +0200

    demo of create snippet!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { FormLoginRegisterComponent } from './features/auth/form-login-register'
 import { Snippets } from './features/snippets/snippets';
 import { FavSnippets } from './features/fav-snippets/fav-snippets';
 import { guardLoginGuard } from './core/auth/guard-login-guard';
+import { CreateSnippet } from './features/create-snippet/create-snippet';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: FormLoginRegisterComponent },
@@ -13,4 +14,9 @@ export const routes: Routes = [
     component: FavSnippets,
     canActivate: [guardLoginGuard],
   },
+  {
+    path: 'create-snippet',
+    component: CreateSnippet,
+    canActivate: [guardLoginGuard],
+  }
 ];
---
+++ src/app/features/create-snippet/components/steps-form/steps-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-steps-form',
+  imports: [],
+  templateUrl: './steps-form.html',
+  styles: ``
+})
+export class StepsForm {
+
+}
