fe26db7f87ed3db6707f468cf868cc7616315ea9
IvanNavas092 <navass21112@gmail.com>
2025-10-13 17:49:10 +0200

    creation form demo for snippet creation

---
+++ src/app/features/create-snippet/form-creation/form-creation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-creation',
+  imports: [],
+  templateUrl: './form-creation.html',
+  styles: ``
+})
+export class FormCreation {
+
+}
