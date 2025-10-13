0dfb495a4d2b6ba953d6092d7e462e8aa8342089
IvanNavas092 <navass21112@gmail.com>
2025-10-13 21:40:38 +0200

    fdf

--- src/app/features/create-snippet/components/steps-form/steps-form.ts
+++ src/app/features/create-snippet/components/steps-form/steps-form.ts
@@ -1,19 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-steps-form',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './steps-form.html',
-  styles: ``
 })
 export class StepsForm {
   @Input() stepActive: number = 1;
+  form!: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      codes: this.fb.array([], Validators.required),
+      
+    })
+  }
+
 
    steps = [
     { number: 1, label: '1. Datos' },
     { number: 2, label: '2. Código' },
     { number: 3, label: '3. Revisión' }
   ];
+
+  createCodeGroup(): FormGroup {
+    return this.fb.group({
+      code
+    })
+
+  }
 
 }
